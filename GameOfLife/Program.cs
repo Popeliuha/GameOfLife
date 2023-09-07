fc1e8e2f854b04a718eb9a9c8ce5a77018192fa6
Tasha Miller <popelyshkon25@gmail.com>
2023-09-07 19:24:26 +0300

    Cleaned all unnessessery console logs

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -17,8 +17,6 @@ namespace GameOfLife
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Console.WriteLine(  "hello");
-            Console.WriteLine("For third commit");
         }
     }
 }
